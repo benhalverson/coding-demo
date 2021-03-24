ec9a828fecad0f136bbf4f130d0e9200b485350c
Ben Halverson <benhalverson33@gmail.com>
2021-03-23 23:45:44 -0700

    init with header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <img className='logo' src='../images/logo.png' alt='company logo' />
+      <nav className='nav-bar'>
+        <ul className='nav-bar-list'>
+          <li>Link</li>
+          <li>Link</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
