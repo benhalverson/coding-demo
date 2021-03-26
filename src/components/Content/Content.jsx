1106aefecd456ca762fbe171036602ff53d925ec
Ben Halverson <benhalverson33@gmail.com>
2021-03-26 01:07:15 -0700

    added new components

---
+++ src/components/Content/Content.jsx
@@ -0,0 +1,89 @@
+import Table from '../Table/Table';
+export default function Content() {
+  return (
+    <>
+      <section className='section'>
+        <div className='section-container'>
+          <h1>Lorem ipsum dolor sit amet</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
+            est pellentesque elit ullamcorper dignissim.
+          </p>
+          <p className='section-text'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+            consectetur purus ut faucibus pulvinar.
+          </p>
+        </div>
+      </section>
+      <section className='section-light-green'>
+        <h1>uspendisse eu elit in turpis eleifend luctus. Nunc congue </h1>
+        <p>
+          Donec rutrum cras eget imperdiet mauris eros fusce, ullamcorper luctus
+          facilisis posuere elementum velit venenatis curae, ridiculus auctor
+          class tristique metus euismod. Eu pulvinar parturient maecenas
+          dictumst iaculis quisque interdum libero tempus, porttitor aenean
+          vulputate commodo risus arcu dictum curabitur luctus aliquam, vivamus
+          taciti convallis nascetur facilisis penatibus lectus nec. Vestibulum
+          scelerisque quam montes potenti faucibus sagittis euismod habitant
+          facilisis, id lobortis luctus mi imperdiet sem litora senectus
+          interdum, ut eros nibh arcu gravida suscipit dictumst vulputate.
+        </p>
+        <p>
+          Etiam dictum cursus quis mus sem scelerisque blandit duis ultrices
+          dictumst vehicula, id fermentum ante commodo et vel ut parturient
+          hendrerit. Blandit justo class semper tincidunt porta potenti, aenean
+          et felis sagittis condimentum congue, luctus fringilla montes in
+          venenatis. Diam porttitor tristique ornare enim sociis senectus duis
+          natoque lobortis turpis porta, est volutpat maecenas velit dis
+          sagittis accumsan lacus convallis montes purus, per ante aliquam
+          platea fringilla nisl tempor sem luctus laoreet. Ultricies blandit
+          aptent tincidunt duis pretium vivamus class per, pharetra fames metus
+          erat id nascetur nullam, at leo phasellus nisl arcu porta faucibus.
+        </p>
+        <p>
+          roin aliquam nibh et nunc bibendum fringilla. Pellentesque iaculis
+          accumsan risus, quis imperdiet tellus interdum ac. Nulla sed sapien
+          auctor, hendre
+        </p>
+      </section>
+      <section className='section-dark-green'>
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </h1>
+        <h3> Sed libero enim sed faucibus.</h3>
+        <div className='section-dark-green-container'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Porttitor massa id neque aliquam vestibulum morbi blandit cursus
+            risus. Velit egestas dui id ornare. Eu ultrices vitae auctor eu
+            augue ut. Fermentum posuere urna nec tincidunt praesent. Eros donec
+            ac odio tempor orci.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Porttitor massa id neque aliquam vestibulum morbi blandit cursus
+            risus. Velit egestas dui id ornare. Eu ultrices vitae auctor eu
+            augue ut. Fermentum posuere urna nec tincidunt praesent. Eros donec
+            ac odio tempor orci.
+          </p>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Porttitor
+          massa id neque aliquam vestibulum morbi blandit cursus risus. Velit
+          egestas dui id ornare. Eu ultrices vitae auctor eu augue ut. Fermentum
+          posuere urna nec tincidunt praesent. Eros donec ac odio tempor orci.
+        </p>
+      </section>
+      <section className='section-yellow'>
+        <h1>uspendisse eu elit in turpis eleifend luctus. Nunc congue</h1>
+        <Table />
+      </section>
+    </>
+  );
+}
---
+++ src/components/RainbowBox/RainbowBox.jsx
@@ -0,0 +1,12 @@
+import './RainbowBox.scss';
+export default function RainbowBox() {
+  return (
+    <div class='grid-container'>
+      <div class='top-bar'></div>
+      <div class='right-side'></div>
+      <div class='bottom-bar'></div>
+      <div class='left-side'></div>
+      <div class='content'>hello</div>
+    </div>
+  );
+}
