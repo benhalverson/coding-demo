142e292a8a4695f966e75c5195a4fc2480eaf6c7
Ben Halverson <benhalverson33@gmail.com>
2021-03-29 01:42:54 -0700

    style(rainbow box): styling

--- src/components/RainbowBox/RainbowBox.jsx
+++ src/components/RainbowBox/RainbowBox.jsx
@@ -1,12 +1,20 @@
 import './RainbowBox.scss';
 export default function RainbowBox() {
   return (
-    <div className='grid-container'>
-      <div className='top-bar'></div>
-      <div className='right-side'></div>
-      <div className='bottom-bar'></div>
-      <div className='left-side'></div>
-      <div className='content'>hello</div>
+    <div class='grid-container'>
+      <div class='orange'></div>
+      <div class='red'></div>
+      <div class='blue'></div>
+      <div class='dark-green'></div>
+      <div class='light-green'></div>
+      <div class='yellow'></div>
+      <div class='orange2'></div>
+      <div class='red2'></div>
+      <div class='blue2'></div>
+      <div class='dark-green2'></div>
+      <div class='light-green2'></div>
+      <div class='yellow2'></div>
+      <div class='white'></div>
     </div>
   );
 }
